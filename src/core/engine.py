12170ab1ed2a58e6fe6998a2081a9e4736e368c0
James Wol <james.wol@petalmail.com>
2026-04-21 04:26:35 +0200

    Update engine.py

--- src/core/engine.py
+++ src/core/engine.py
@@ -2,17 +2,14 @@
 
 from src.core.mock_llm import MockLLM
 from src.core.llm_agent import LLMAgent
-from src.core.council_engine import CouncilEngine
-from src.core.iteration_engine import IterationEngine
+from src.core.debate_engine import DebateEngine
 
 
 class WorkflowEngine:
     def __init__(self):
         self.llm = MockLLM()
         self.agent = LLMAgent(self.llm)
-
-        self.council = CouncilEngine(self.agent)
-        self.iteration = IterationEngine(self.council)
+        self.debate = DebateEngine(self.agent)
 
         self.context = {}
 
@@ -20,4 +17,4 @@ class WorkflowEngine:
         self.context[key] = value
 
     def run_workflow(self, workflow_name=None):
-        return self.iteration.run_iterations(self.context, rounds=3)
+        return self.debate.run_debate(self.context)
