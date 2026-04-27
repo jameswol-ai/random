9746988e0a1ec6d9cac55397856810f5b9e4893a
James Wol <james.wol@petalmail.com>
2026-04-27 18:51:10 +0200

    Update context.py

--- src/core/context.py
+++ src/core/context.py
@@ -1,11 +1,20 @@
 # src/core/context.py
 
+from models.design_schema import create_empty_design
+
 class Context:
     def __init__(self, data=None):
         self.data = data or {}
+        self.data["design"] = create_empty_design()
 
     def update(self, key, value):
         self.data[key] = value
 
+    def update_design(self, key, value):
+        self.data["design"][key] = value
+
     def get(self, key, default=None):
         return self.data.get(key, default)
+
+    def get_design(self):
+        return self.data["design"]
